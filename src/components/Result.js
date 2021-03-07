b23bd79bed4c4ac255144c5c1b1a7b0e9264065a
Santiago Martin <thesantiagomartin@gmail.com>
2021-03-07 13:02:59 -0800

    result displays user

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,43 +1,52 @@
 import React from 'react';
 
 const StatusDisplay = props => {
-    if (props.splitsheetId) {
-        return (
-            <div>
-                <h1>Form Submission Succeeded</h1>
-                <p>
-                    form successfully posted w/ splitsheet ID {props.splitsheetId}
-                </p>
-            </div>
+  if (props.splitsheetId) {
+    return (
+      <div>
+        <h1>Form Submission Succeeded</h1>
+        <p>
+          form successfully posted w/ splitsheet ID {props.splitsheetId}
+        </p>
+      </div>
         );
-    } else {
-        return (
-            <div>
-                <h1>Failed to Post</h1>
-                <p>
-                    status code {props.errorCode}
-                    <br />
-                    message {props.errorMessage}
-                </p>
-            </div>
-        );
-    }
+  } else {
+    return (
+      <div>
+        <h1>Failed to Post</h1>
+        <p>
+          status code {props.errorCode}
+          <br />
+          message {props.errorMessage}
+        </p>
+      </div>
+    );
+  }
 }
 
 const Result = props => (
-    <div>
-        <StatusDisplay splitsheetId={props.splitsheetId}
-          errorCode={props.errorCode}
-          errorMessage={props.errorMessage} />
-        <ul>
-            <li>Song Title: {props.splitsheet.song_title}</li>
-            <li>Signed Date: {props.splitsheet.signed_date}</li>
-            <li>Location: {props.splitsheet.location}</li>
-        </ul>
-        <button onClick={props.onReturn}>
-            Return to Form
-        </button>
-    </div>
+  <div>
+    <StatusDisplay splitsheetId={props.splitsheetId}
+      errorCode={props.errorCode}
+      errorMessage={props.errorMessage} />
+    <ul>
+      <li>Song Title: {props.splitsheet.song_title}</li>
+      {
+        props.splitsheet.users.map((value, index) => {
+          return (
+            <div key={index}>
+              <li>User #{index + 1} First Name: {value.first_name}</li>
+            </div>
+          )
+        })
+      }
+      <li>Signed Date: {props.splitsheet.signed_date}</li>
+      <li>Location: {props.splitsheet.location}</li>
+    </ul>
+    <button onClick={props.onReturn}>
+      Return to Form
+    </button>
+  </div>
 );
 
 export default Result;
