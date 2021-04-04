9e0ba32904fedf4cf38e93afa355b69f65e09eab
Santiago Martin <thesantiagomartin@gmail.com>
2021-04-04 14:51:02 -0700

    clean up form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const FormInput = props => {
+  return (
+    <div>
+      <label>{props.title}:</label>
+      <input type={props.inputType}
+        value={props.defaultValue}
+        onChange={event => props.onChange(event)} />
+    </div>
+  )
+}
+
 class Form extends React.Component {
   constructor(props) {
     super(props);
@@ -34,107 +45,72 @@ class Form extends React.Component {
     });
   }
 
+  createUserInputOnChangeCallback = (index, field) => {
+    return event => {
+      this.setState(prevState => {
+        const newState = { users: [...prevState.users] };
+        newState.users[index][field] = event.target.value;
+        return newState;
+      });
+    };
+  }
+
   render() {
     return (
       <div>
         <h1>Make a Splitsheet</h1>
         <form onSubmit={this.handleSubmit}>
-          <label>Song Title:</label>
-          <input type="text"
-            value={this.state.song_title}
+          <FormInput title="Song Title"
+            inputType="text"
+            defaultValue={this.state.song_title}
             onChange={(event) => this.setState({song_title: event.target.value})} />
-          <br />
           {
-            this.state.users.map((value, index) => {
-              return (
-                <div key={index}>
-                  <label>User #{index + 1} First Name:</label>
-                  <input type="text"
-                    value={value.first_name}
-                    onChange={(event) => {
-                      this.setState(prevState => {
-                        const newState = { users: [...prevState.users] };
-                        newState.users[index].first_name = event.target.value;
-                        return newState;
-                      });
-                    }} />
-                  <br />
-                  <label>User #{index + 1} Last Name:</label>
-                  <input type="text"
-                    value={value.last_name}
-                    onChange={(event) => {
-                      this.setState(prevState => {
-                        const newState = { users: [...prevState.users] };
-                        newState.users[index].last_name = event.target.value;
-                        return newState;
-                      });
-                    }} />
-                  <br />
-                  <label>User #{index + 1} Email:</label>
-                  <input type="email"
-                    value={value.email}
-                    onChange={(event) => {
-                      this.setState(prevState => {
-                        const newState = { users: [...prevState.users] };
-                        newState.users[index].email = event.target.value;
-                        return newState;
-                      });
-                    }} />
-                  <br />
-                  <label>User #{index + 1} Phone Number:</label>
-                  <input type="tel"
-                    value={value.phone_number}
-                    onChange={(event) => {
-                      this.setState(prevState => {
-                        const newState = { users: [...prevState.users] };
-                        newState.users[index].phone_number = event.target.value;
-                        return newState;
-                      });
-                    }} />
-                  <br />
-                  <label>User #{index + 1} Ownership Percent:</label>
-                  <input type="number"
-                    value={value.ownership_percentage}
-                    onChange={(event) => {
-                      this.setState(prevState => {
-                        const newState = { users: [...prevState.users] };
-                        newState.users[index].ownership_percentage = event.target.value;
-                        return newState;
-                      });
-                    }} />
-                  <br />
-                  <label>User #{index + 1} Signature:</label>
-                  <input type="text"
-                    value={value.signature}
-                    onChange={(event) => {
-                      this.setState(prevState => {
-                        const newState = { users: [...prevState.users] };
-                        newState.users[index].signature = event.target.value;
-                        return newState;
-                      });
-                    }} />
+            this.state.users.map((value, index) => (
+              <div key={index}>
+                <p>User #{index + 1}</p>
+                <div>
+                  <FormInput title="First Name"
+                    inputType="text"
+                    defaultValue={value.first_name}
+                    onChange={this.createUserInputOnChangeCallback(index, 'first_name')} />
+                  <FormInput title="Last Name"
+                    inputType="text"
+                    defaultValue={value.last_name}
+                    onChange={this.createUserInputOnChangeCallback(index, 'last_name')} />
+                  <FormInput title="Email"
+                    inputType="email"
+                    defaultValue={value.email}
+                    onChange={this.createUserInputOnChangeCallback(index, 'email')} />
+                  <FormInput title="Phone Number"
+                    inputType="tel"
+                    defaultValue={value.phone_number}
+                    onChange={this.createUserInputOnChangeCallback(index, 'phone_number')} />
+                  <FormInput title="Ownership Percentage"
+                    inputType="number"
+                    defaultValue={value.ownership_percentage}
+                    onChange={this.createUserInputOnChangeCallback(index, 'ownership_percentage')} />
+                  <FormInput title="Signature"
+                    inputType="text"
+                    defaultValue={value.signature}
+                    onChange={this.createUserInputOnChangeCallback(index, 'signature')} />
                 </div>
-              )
-            })
+              </div>
+            ))
           }
           <button onClick={this.addUser}>
             Add User
           </button>
-          <br />
-          <label>Signed Date:</label>
-          <input type="date"
-            value={this.state.signed_date}
-            onChange={(event) => this.setState({signed_date: event.target.value})} />
-          <br />
-          <label>Location:</label>
-          <input type="text"
-            value={this.state.location}
-            onChange={(event) => this.setState({location: event.target.value})} />
-          <br />
+          <FormInput title="Signed Date"
+            inputType="text"
+            defaultValue={this.state.signed_date}
+            onChange={event => this.setState({signed_date: event.target.value})} />
+          <FormInput title="Location"
+            inputType="text"
+            defaultValue={this.state.location}
+            onChange={event => this.setState({location: event.target.value})} />
           <input type="submit" value="Submit" />
         </form>
       </div>
-
     );
   }
 }
